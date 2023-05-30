9791373e69d1ccac19eb3d069905b866857236c6
DaniFort <danifortvideogamedeveloper@gmail.com>
2023-05-30 14:28:43 +0200

    Creating project

---
+++ CoweDefense/Source/CoweDefense/CoweDefense.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CoweDefense.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CoweDefense, "CoweDefense" );
