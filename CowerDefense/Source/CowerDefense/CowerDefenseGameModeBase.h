9f481991be017bd75cc9da5f982d5ec005d7d59b
Darío <115427894+SpaceBoyDev@users.noreply.github.com>
2023-05-31 19:10:15 +0200

    Initial Commit
    gitignore otra vez y todo eso

---
+++ CowerDefense/Source/CowerDefense/CowerDefenseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CowerDefenseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COWERDEFENSE_API ACowerDefenseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
