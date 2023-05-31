888dfeb014cb4f626995423e3ac6e5d5998145db
DaniFort <danifortvideogamedeveloper@gmail.com>
2023-05-31 18:42:19 +0200

    Borrado

--- CowerDefensePR/Intermediate/Build/Win64/CowerDefensePREditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30148
-#include "D:/Documentos/TheCoreSchool/PI3/Project/cowerdefense/CowerDefensePR/Intermediate/Build/Win64/CowerDefensePREditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- CowerDefensePR/Source/CowerDefensePR/CowerDefensePR.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CowerDefensePR.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CowerDefensePR, "CowerDefensePR" );
